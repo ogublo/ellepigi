9bd57b9290f36e1fdfc742762085ddc6bf112825
ogublo <166822486+ogublo@users.noreply.github.com>
2024-04-13 04:32:29 +0200

    Add files via upload

---
+++ res/search.inc.php
@@ -0,0 +1,56 @@
+<?php
+
+/*****************
+	GENERAL SETTINGS
+*****************/
+$imSettings['search']['general'] = array(
+	'menu_position' => 'left',
+	'defaultScope' => array(
+		'0' => 'index.html',
+		'3' => 'video.html',
+		'4' => 'foto.html',
+		'5' => 'documenti.html',
+		'10' => 'altro.html',
+		'11' => 'video-rita.html',
+		'12' => 'foto-rita.html',
+		'13' => 'documenti-rita.html',
+		'14' => 'altro-rita.html',
+		'15' => 'video-carol.html',
+		'16' => 'foto-carol.html',
+		'17' => 'documenti-carol.html',
+		'18' => 'altro-carol.html',
+		'19' => 'video-mari.html',
+		'20' => 'foto-mari.html',
+		'21' => 'documenti-mari.html',
+		'22' => 'altro-mari.html',
+		'24' => 'video-di-tutti.html',
+		'25' => 'foto-di-tutti.html',
+		'26' => 'documenti-di-tutti.html',
+		'27' => 'altro-di-tutti.html'
+	),
+	'extendedScope' => array(
+	)
+);
+
+/*****************
+	PRODUCTS SEARCH
+*****************/
+$imSettings['search']['products'] = array(
+);
+
+/*****************
+	IMAGES SEARCH
+*****************/
+$imSettings['search']['images'] = array(
+);
+
+/*****************
+	VIDEOS SEARCH
+*****************/
+$imSettings['search']['videos'] = array(
+);
+$imSettings['search']['dynamicobjects'] = array(
+
+);
+
+// End of file search.inc.php
---
+++ res/x5settings.php
@@ -0,0 +1,119 @@
+<?php
+
+/*
+|-------------------------------
+|	GENERAL SETTINGS
+|-------------------------------
+*/
+
+$imSettings['general'] = array(
+	'site_id' => 'A36A21F3226196625055B2EA59AC7F52',
+	'url' => 'http://localhost/',
+	'homepage_url' => 'http://localhost/index.html',
+	'icon' => '',
+	'version' => '2022.2.11.0',
+	'sitename' => 'NUVOLA',
+	'lang_code' => 'it-IT',
+	'public_folder' => '',
+	'salt' => 'l9jesw1p1cro77xtewz301hk1kl55e7fmxlrq1cr3p9hk1q1fcd',
+	'common_email_sender_addres' => ''
+);
+/*
+|-------------------------------
+|	PASSWORD POLICY
+|-------------------------------
+*/
+
+$imSettings['password_policy'] = array(
+	'required_policy' => false,
+	'minimum_characters' => '6',
+	'include_uppercase' => false,
+	'include_numeric' => false,
+	'include_special' => false
+);
+/*
+|-------------------------------
+|	Captcha
+|-------------------------------
+*/ImTopic::$captcha_code = "		<div class=\"x5captcha-wrap\">
+			<label for=\"pepi2t7z-imCpt\">Parola di controllo:</label><br />
+			<input type=\"text\" id=\"pepi2t7z-imCpt\" class=\"imCpt\" name=\"imCpt\" maxlength=\"5\" />
+		</div>
+";
+
+
+$imSettings['admin'] = array(
+	'icon' => 'admin/images/logo_dthmjsyc.png',
+	'theme' => 'orange',
+	'extra-dashboard' => array(),
+	'extra-links' => array()
+);
+
+
+/*
+|--------------------------------------------------------------------------------------
+|	DATABASES SETTINGS
+|--------------------------------------------------------------------------------------
+*/
+
+$imSettings['databases'] = array();
+$ecommerce = Configuration::getCart();
+$ecommerce->setSettings(array(
+	'page_url' => 'http://localhost/',
+	'force_sender' => false,
+	'mail_btn_css' => 'display: inline-block; text-decoration: none; color: rgba(255, 255, 255, 1); background-color: rgba(54, 71, 79, 1); padding: 10px 10px 10px 10px; border-style: solid; border-width: 0px 0px 0px 0px; border-color: rgba(55, 71, 79, 1) rgba(55, 71, 79, 1) rgba(55, 71, 79, 1) rgba(55, 71, 79, 1); border-top-left-radius: 3px; border-top-right-radius: 3px; border-bottom-left-radius: 3px; border-bottom-right-radius: 3px;',
+	'email_opening' => '',
+	'email_closing' => '',
+	'useCSV' => false,
+	'header_bg_color' => 'rgba(37, 58, 88, 1)',
+	'header_text_color' => 'rgba(255, 255, 255, 1)',
+	'cell_bg_color' => 'rgba(255, 255, 255, 1)',
+	'cell_text_color' => 'rgba(0, 0, 0, 1)',
+	'border_color' => 'rgba(211, 211, 211, 1)',
+	'owner_email' => 'example@example.com',
+	'vat_type' => 'included',
+	'availability_image' => ''
+));
+
+$ecommerce->setPriceFormatData(array(
+	'decimals' => 2,
+	'decimal_sep' => '.',
+	'thousands_sep' => '',
+	'currency_to_right' => true,
+	'currency_separator' => ' ',
+	'show_zero_as' => '0',
+	'currency_symbol' => '€',
+	'currency_code' => 'EUR',
+	'currency_name' => 'Euro',
+));
+
+
+/*
+|-------------------------------------------------------------------------------------------
+|	GUESTBOOK SETTINGS
+|-------------------------------------------------------------------------------------------
+*/
+
+$imSettings['guestbooks'] = array();
+
+
+/*
+|-------------------------------
+|	EMAIL SETTINGS
+|-------------------------------
+*/
+
+$ImMailer->emailType = 'phpmailer';
+$ImMailer->header = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">' . "\n" . '<html>' . "\n" . '<head>' . "\n" . '<meta http-equiv="content-type" content="text/html; charset=utf-8">' . "\n" . '<meta name="generator" content="Incomedia WebSite X5 Evolution 2022.2.11 - www.websitex5.com">' . "\n" . '</head>' . "\n" . '<body bgcolor="#37474F" style="background-color: #37474F;">' . "\n\t" . '<table border="0" cellpadding="0" align="center" cellspacing="0" style="padding: 0; margin: 0 auto; width: 700px;">' . "\n\t" . '<tr><td id="imEmailContent" style="min-height: 300px; padding: 10px; font: normal normal normal 12pt \'Tahoma\'; color: #000000; background-color: #FFFFFF; text-decoration: none; text-align: left; width: 700px; border-style: solid; border-color: #000000; border-top-width: 1px; border-right-width: 1px; border-bottom-width: 1px; border-left-width: 1px; background-color: #FFFFFF" width="700px">' . "\n\t\t";
+$ImMailer->footer = "\n\t" . '</td></tr>' . "\n\t" . '</table>' . "\n" . '<table width="100%"><tr><td id="imEmailFooter" style="font: normal normal normal 9pt \'Tahoma\'; color: #FFFFFF; background-color: transparent; text-decoration: none; text-align: center;  padding: 10px; margin-top: 5px;background-color: transparent">' . "\n\t\t" . 'Questo messaggio di posta elettronica contiene informazioni rivolte esclusivamente al destinatario sopra indicato.<br>Nel caso aveste ricevuto questo messaggio di posta elettronica per errore, siete pregati di segnalarlo immediatamente al mittente e distruggere quanto ricevuto senza farne copia.' . "\n\t" . '</td></tr></table>' . "\n\t" . '</body>' . "\n" . '</html>';
+$ImMailer->bodyBackground = '#FFFFFF';
+$ImMailer->bodyBackgroundEven = '#FFFFFF';
+$ImMailer->bodyBackgroundOdd = '#F0F0F0';
+$ImMailer->bodyBackgroundBorder = '#CDCDCD';
+$ImMailer->bodyTextColorOdd = '#000000';
+$ImMailer->bodySeparatorBorderColor = '#000000';
+$ImMailer->emailBackground = '#37474F';
+$ImMailer->emailContentStyle = 'font: normal normal normal 12pt \'Tahoma\'; color: #000000; background-color: #FFFFFF; text-decoration: none; text-align: left; ';
+$ImMailer->emailContentFontFamily = 'font-family: Tahoma;';
+
+// End of file x5settings.php
